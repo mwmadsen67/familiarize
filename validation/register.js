3a4fb584a15755955e7c1808322a97f0a19b561d
Carlos Garcia <carlosaicrag@gmail.com>
2019-11-19 18:06:38 -0800

    setup user auth

--- validation/register.js
+++ validation/register.js
@@ -3,7 +3,7 @@ const validText = require('./valid-text');
 
 module.exports = function validateRegisterInput(data) {
     let errors = {};
-
+    
     data.username = validText(data.username) ? data.username : '';
     data.email = validText(data.email) ? data.email : '';
     data.password = validText(data.password) ? data.password : '';
