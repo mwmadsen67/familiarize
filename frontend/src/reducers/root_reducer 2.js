d6e32e6ea7d234fa4bcf89842cc9b4f9964dd173
Carlos Garcia <carlosaicrag@gmail.com>
2019-11-20 11:26:37 -0800

    remove duplicate files

--- frontend/src/reducers/root_reducer 2.js
+++
@@ -1,8 +0,0 @@
-import {combineReducers} from "redux"
-import sessionReducer from "./session_reducer"
-import ErrorsReducer from "./errors_reducer"
-const RootReducer = combineReducers({
-    session: sessionReducer,
-    errors: ErrorsReducer
-})
-export default RootReducer
